0f9f2f647f6cfe3a78aa04f07a1f5f33d5108211
mherrera491 <mattherrera1@gmail.com>
2024-01-29 18:46:52 -0800

    created about path

--- vinylcollector/main_app/views.py
+++ vinylcollector/main_app/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 
 def home(request):
     return HttpResponse('<h1>Hello, World!</h1>')
+
+def about(request):
+    return HttpResponse('<h1>All About the Vinyl Collector</h1>')
