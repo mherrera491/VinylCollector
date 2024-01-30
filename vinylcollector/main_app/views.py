4b9fcd853e8a9eda77c91e68be8411671fd10607
mherrera491 <mattherrera1@gmail.com>
2024-01-29 19:16:47 -0800

    added rendered templates, index, and css

--- vinylcollector/main_app/views.py
+++ vinylcollector/main_app/views.py
@@ -3,7 +3,24 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse('<h1>Hello, World!</h1>')
+    return render(request, 'home.html')
 
 def about(request):
-    return HttpResponse('<h1>All About the Vinyl Collector</h1>')
+    return render(request, 'about.html')
+
+class Vinyl:
+    def __init__(self, album, artist, release_year, genre):
+        self.album = album
+        self.artist = artist
+        self.release_year = release_year
+        self.genre = genre
+
+vinyls = [
+    Vinyl('Swimming', 'Mac Miller', '2018', 'Hip-Hop/Rap'),
+    Vinyl('Thee Sacred Souls', 'Thee Sacred Souls', '2022', 'R&B/Soul'),
+    Vinyl('Ctrl', 'SZA', '2017', 'R&B'),
+]
+
+def vinyl_index(request):
+    return render(request, 'vinyl/index.html', { 'vinyls': vinyls })
+        
